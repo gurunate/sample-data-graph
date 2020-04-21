4fabb9a5fa9da4c754c7f6b3f0671c5fb84cd2a5
Nate Johnson <nathanalanjohnson@gmail.com>
2020-04-20 19:36:27 -0500

    Rename

--- src/index.js
+++ src/index.js
@@ -9,20 +9,38 @@ const typeDef = gql`
 `;
 
 const server = new ApolloServer({
-    typeDefs: [
-        typeDef,
-        users.typeDef,
-        todos.typeDef,
-    ],
-    resolvers: [
-        users.resolvers,
-        todos.resolvers
-    ],
+    context: ({ req }) => {
+        // Note! This example uses the `req` object to access headers,
+        // but the arguments received by `context` vary by integration.
+        // This means they will vary for Express, Koa, Lambda, etc.!
+        //
+        // To find out the correct arguments for a specific integration,
+        // see the `context` option in the API reference for `apollo-server`:
+        // https://www.apollographql.com/docs/apollo-server/api/apollo-server/
+
+        // Get the user token from the headers.
+
+        // try to retrieve a user with the token
+        // const user = getUser(token);
+
+        const user = {};
+
+        // add the user to the context
+        return { user };
+    },
+    typeDefs: [typeDef, users.typeDef, todos.typeDef],
+    resolvers: [users.resolvers, todos.resolvers],
     dataSources: () => ({
         UsersAPI: new users.UsersAPI(),
-        TodosAPI: new todos.TodosAPI(),
+        TodosAPI: new todos.TodosAPI()
     })
 });
+
+// const app = express();
+// // Additional middleware can be mounted at this point to run before Apollo.
+// app.use('*', jwtCheck, requireAuth, checkScope);
+
+// server.applyMiddleware({ app, path: '/specialUrl' }); 
 
 server.listen().then(({ url }) => {
     console.log(`🚀  Server ready at ${url}`);
